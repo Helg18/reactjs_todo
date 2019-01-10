7dea3ff30f592f176690a69bbfcd8782533350f7
Henry Leon <helg18@gmail.com>
2019-01-10 00:14:45 -0400

    Incluyendo FontAwesome | Quitando Css no usado | Registrando FontAwesome | Escribiendo un nav para la app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <nav>
+          <ul className="nav justify-content-center">
+            <li className="nav-item">
+              <a className="nav-link active" href="#"><i className="fa fa-sticky-note"></i> Tareas</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#"><i className="fa fa-plus"></i> Nueva</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     );
   }
