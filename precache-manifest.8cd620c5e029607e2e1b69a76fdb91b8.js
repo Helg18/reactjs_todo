35af5b017422a4372dddbf19b9b2514ccc4b8822
Henry Leon Gomez <helg18@gmail.com>
2019-08-05 12:51:52 -0400

    Updates

---
+++ precache-manifest.8cd620c5e029607e2e1b69a76fdb91b8.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "68f81c99bb5365b25418",
+    "url": "/reactjs_todo/static/css/main.cd7a7c2a.chunk.css"
+  },
+  {
+    "revision": "68f81c99bb5365b25418",
+    "url": "/reactjs_todo/static/js/main.68f81c99.chunk.js"
+  },
+  {
+    "revision": "5d4749dcd3a89addfadf",
+    "url": "/reactjs_todo/static/css/1.2b14a318.chunk.css"
+  },
+  {
+    "revision": "5d4749dcd3a89addfadf",
+    "url": "/reactjs_todo/static/js/1.5d4749dc.chunk.js"
+  },
+  {
+    "revision": "02b21ca093a66e7b7f74",
+    "url": "/reactjs_todo/static/js/runtime~main.02b21ca0.js"
+  },
+  {
+    "revision": "e65d96d04378217b8b5caff2fb1b62cb",
+    "url": "/reactjs_todo/static/media/fa-regular-400.e65d96d0.woff2"
+  },
+  {
+    "revision": "fc5252e568503bfb163233ec63b8a2ce",
+    "url": "/reactjs_todo/static/media/fa-regular-400.fc5252e5.woff"
+  },
+  {
+    "revision": "8e9f0ee6d016526776c27f91fea55cca",
+    "url": "/reactjs_todo/static/media/fa-regular-400.8e9f0ee6.eot"
+  },
+  {
+    "revision": "deb8619e67992d3fa088b5dcbb8cff36",
+    "url": "/reactjs_todo/static/media/fa-regular-400.deb8619e.ttf"
+  },
+  {
+    "revision": "b89c6c51aa2999159a6667f97a46a346",
+    "url": "/reactjs_todo/static/media/fa-brands-400.b89c6c51.woff2"
+  },
+  {
+    "revision": "8086e197694282b26c7b729945ecc377",
+    "url": "/reactjs_todo/static/media/fa-solid-900.8086e197.woff2"
+  },
+  {
+    "revision": "dd4ff1196905f0585aef2c80486869e9",
+    "url": "/reactjs_todo/static/media/fa-brands-400.dd4ff119.woff"
+  },
+  {
+    "revision": "52b8345b6d8e11988d8db917c366b4da",
+    "url": "/reactjs_todo/static/media/fa-solid-900.52b8345b.woff"
+  },
+  {
+    "revision": "7d43b9ae0b8bb1d0dc425d78dc577b0a",
+    "url": "/reactjs_todo/static/media/fa-brands-400.7d43b9ae.eot"
+  },
+  {
+    "revision": "50a544b5ad4d512e8db3cdc089823a00",
+    "url": "/reactjs_todo/static/media/fa-brands-400.50a544b5.ttf"
+  },
+  {
+    "revision": "734248d027cbaac7f03c0d931ec95593",
+    "url": "/reactjs_todo/static/media/fa-regular-400.734248d0.svg"
+  },
+  {
+    "revision": "f72d546abcc27880adf8428792c72519",
+    "url": "/reactjs_todo/static/media/fa-solid-900.f72d546a.eot"
+  },
+  {
+    "revision": "1709a2810c3752483683f3f7d287aa89",
+    "url": "/reactjs_todo/static/media/fa-solid-900.1709a281.ttf"
+  },
+  {
+    "revision": "fef43c1fd872ef7f1880483d9b63ccf2",
+    "url": "/reactjs_todo/static/media/fa-brands-400.fef43c1f.svg"
+  },
+  {
+    "revision": "27603739fe78fee587c7b84873475b61",
+    "url": "/reactjs_todo/static/media/fa-solid-900.27603739.svg"
+  },
+  {
+    "revision": "5472ee5edb8ac6a1608bce4bc21465bf",
+    "url": "/reactjs_todo/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/reactjs_todo/precache-manifest.8cd620c5e029607e2e1b69a76fdb91b8.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/reactjs_todo/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
